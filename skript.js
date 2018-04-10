4f1543e92059f668d670fb70b2764259b88f9b09
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2018-04-10 21:58:20 +0200

    correcting esLint warnigs

--- skript.js
+++ skript.js
@@ -1,30 +1,27 @@
-let x = 0,
-    y = 0,
-    spacing = 30,
-    fps = 255;
+let x = 0;
+let y = 0;
+const spacing = 30;
+const fps = 255;
 
 function setup() {
-    let w = document.body.clientWidth;
-    // let h = document.body.clientHeight;
-    createCanvas(w, 800);
-    // createCanvas(w, h);
-    background(255);
+  const w = document.body.clientWidth;
+  // let h = document.body.clientHeight;
+  createCanvas(w, 800);
+  // createCanvas(w, h);
+  background(255);
 }
 
 function draw() {
-    stroke(0);
-    if (random(1) < 0.5) {
-        line(x, y, x + spacing, y + spacing);
-        frameRate(fps);
-
-    } else {
-        line(x, y + spacing, x + spacing, y);
-
-    }
-    x += spacing;
-    if (x > width) {
-        x = 0;
-        y += spacing;
-    }
-
+  stroke(0);
+  if (random(1) < 0.5) {
+    line(x, y, x + spacing, y + spacing);
+    frameRate(fps);
+  } else {
+    line(x, y + spacing, x + spacing, y);
+  }
+  x += spacing;
+  if (x > width) {
+    x = 0;
+    y += spacing;
+  }
 }
